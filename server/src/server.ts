23091e4f50fca10879f8501b34193fa4f0dcba52
DIPAKK2310 <dipakkhare217@gmail.com>
2026-03-24 00:24:36 +0530

    fix:client url fix

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,7 +10,7 @@ app.use(express.json());
 const corsOption: CorsOptions = {
   origin:[
  "http://localhost:5173",
-   "https://dipnext.vercel.app"],
+   "https://nike-landing-vert-nine.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
