a17f40017d0b304f0d404d3af767b86cb914418a
DIPAKK2310 <dipakkhare217@gmail.com>
2026-03-24 08:23:22 +0530

    fix:Extra width on scroller and the flutter of images

--- client/src/components/products/Products_Scroll.jsx
+++ client/src/components/products/Products_Scroll.jsx
@@ -2,11 +2,11 @@ import React, { useState } from 'react'
 import { products } from "../../constants/index.js";
 
 const ProductsScroll = () => {
-    const extendedProducts = [...products, ...products];
+    const extendedProducts = [...products,...products];
   return (
-<div className="mt-12">
+<div className="mt-12 overflow-hidden">
 
-  <div className="flex items-center gap-6 animate-scroll animate-scroll:hover">
+  <div className="flex w-max items-center gap-6 animate-scroll animate-scroll:hover">
     {extendedProducts.map((item, i) => (
       <div key={i} className="min-w-[200px] bg-white p-4 rounded-xl">
         <img src={item.imgURL} className="h-full mx-auto" />
